ef6d4cf25cbe324f8b0edb6399e7b1802b7852db
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-10-22 22:15:16 -0400

    Update TwoSum.py

--- LeetCode/Arrays/Easy/TwoSum.py
+++ LeetCode/Arrays/Easy/TwoSum.py
@@ -3,8 +3,8 @@ It turns out we can do it in one-pass. While we are iterating and inserting elem
 we also look back to check if current element's complement already exists in the hash table. If it exists, 
 we have found a solution and return the indices immediately.
 
-Time complexity: O(n)O(n). We traverse the list containing nn elements only once. Each lookup in the table costs only O(1)O(1) time.
-Space complexity: O(n)O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
+Time complexity: O(n). We traverse the list containing nn elements only once. Each lookup in the table costs only O(1) time.
+Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
 """
 
 class Solution:
