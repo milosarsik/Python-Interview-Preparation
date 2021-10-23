003b6367c3f444aad69e7c3e0743e5ddf17c4839
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-10-23 12:48:11 -0400

    Update ProductOfArrayExceptSelf.py

--- LeetCode/Arrays/Medium/ProductOfArrayExceptSelf.py
+++ LeetCode/Arrays/Medium/ProductOfArrayExceptSelf.py
@@ -1,2 +1,20 @@
 """
+Time complexity: O(n). We are passing once.
+Space complexity: O(1). Because the output array does not count as space
 """
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+            
+        return res
