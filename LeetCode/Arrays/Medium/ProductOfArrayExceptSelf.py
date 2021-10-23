67afa639810b14defc1028bed2fed1da192eccdc
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-10-23 12:48:57 -0400

    Update ProductOfArrayExceptSelf.py

--- LeetCode/Arrays/Medium/ProductOfArrayExceptSelf.py
+++ LeetCode/Arrays/Medium/ProductOfArrayExceptSelf.py
@@ -1,4 +1,8 @@
 """
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
 Time complexity: O(n). We are passing once.
 Space complexity: O(1). Because the output array does not count as space
 """
