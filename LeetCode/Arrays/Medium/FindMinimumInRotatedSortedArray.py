0552601b78fc7d03f1cd60459c7c838f8d075cfd
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-10-25 22:01:22 -0400

    Create FindMinimumInRotatedSortedArray.py

---
+++ LeetCode/Arrays/Medium/FindMinimumInRotatedSortedArray.py
@@ -0,0 +1,34 @@
+"""
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+Time complexity: O(nlogn). We are doing a binary search because the array is sorted
+Space complexity: O(1). We are not using an array, we are just storing in single variables
+
+"""
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+        
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return res
