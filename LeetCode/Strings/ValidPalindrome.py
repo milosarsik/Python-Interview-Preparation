16400602cd022fcb606f6eb729077201401232ac
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-11-08 22:08:42 -0500

    Create ValidPalindrome.py

---
+++ LeetCode/Strings/ValidPalindrome.py
@@ -0,0 +1,53 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all 
+non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Solution #1
+Time complexity: O(n). 
+Space complexity: O(n). We are using extra memor when reversing the string at the end
+
+Solution #2
+Time complexity: O(n). 
+Space complexity: O(1). No extra space needed 
+"""
+
+# Solution #1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
+        
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+            
+        return newStr == newStr[::-1]
+      
+# Solution #2
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l, r = l + 1, r - 1
+            
+        return True
+    
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
