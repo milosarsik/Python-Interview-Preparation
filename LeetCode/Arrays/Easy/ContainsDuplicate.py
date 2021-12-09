a389870666605f079e4e13a94ea8a682a7afca90
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-12-09 13:09:23 -0500

    Update ContainsDuplicate.py

--- LeetCode/Arrays/Easy/ContainsDuplicate.py
+++ LeetCode/Arrays/Easy/ContainsDuplicate.py
@@ -6,3 +6,15 @@ Space complexity: O(n).
 """
 def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return False
+
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+
+        return False
