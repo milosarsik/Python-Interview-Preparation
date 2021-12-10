c512647f3818763df4a52ae00dd542f9458ed42c
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2021-12-09 22:43:40 -0500

    Create SortColors.py

---
+++ LeetCode/Arrays/Medium/SortColors.py
@@ -0,0 +1,37 @@
+"""
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in 
+the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Time complexity: O(n). Single pass, because we use pointers from left and right
+Space complexity: O(1). In-place
+"""
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, len(nums) - 1
+        i = 0
+        
+        def swap(i, j):
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp
+        
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+                l += 1
+            elif nums[i] == 2:
+                swap(i, r)
+                r -= 1
+                i -= 1  # this will cancel out the below increment when we swap r's
+            i += 1
