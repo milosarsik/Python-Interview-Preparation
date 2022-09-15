5b111e96e3a6045925c59f126455cf5ab50bb43d
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2022-09-15 14:50:20 -0400

    Update ContainsDuplicate.py

--- LeetCode/Arrays/Easy/ContainsDuplicate.py
+++ LeetCode/Arrays/Easy/ContainsDuplicate.py
@@ -1,9 +1,48 @@
 """
-One line solution that uses a set. Set's do not contain duplicates. Length's should be the same if there are no duplicates!
+------------------------------------------------------------------------------------------------------------------------------------------------
+217. Contains Duplicate (Blind)(Leetcode)
 
-Time complexity: O(n).
-Space complexity: O(n).
+Given an INTEGER ARRAY "nums", return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+Solution Walkthrough 
+
+There are two solutions we can code for this problem. There is a simple one line solution, and a longer for loop solution.
+
+The one line solution uses a set. Set's do not contain duplicates. Therefore when we compare the length of the set to the length of the original 
+array; if they are the same length, there are no duplicates, if they differ in length, there were duplicates and they were removed when we created
+the set. 
+
+For the longer solution, istantiate a SET. Iterate through the list and check if current "num" is in the SET. 
+If it is, return true because it contains a duplicate. If it is not in the set, add it to the set and the loop continues to the next value. 
+If the loop finishes, return false because we have not found a duplicate value.
+
+Ex. nums = [1,2,3,1]
+
+hashset = [1,2,3]
+
+num
+1
+2
+3
+1
+
+Return: true
+
+Time complexity: O(n). We traverse the list containing n elements only once.
+Space complexity: O(n). Worst case we have to create space for every element in the list, therefore it is an O(n) space complexity. 
 """
+
 def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
 
