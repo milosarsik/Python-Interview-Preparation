12fe4f1b7bc354d587d70d6d293c74e476bb2c71
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2022-11-14 15:48:53 -0500

    Update 155. MinStack.py

--- LeetCode/Stacks/Medium/155. MinStack.py
+++ LeetCode/Stacks/Medium/155. MinStack.py
@@ -3,6 +3,37 @@
 155. Min Stack (EPI)(Blind)(LeetCode)
 ------------------------------------------------------------------------------------------------------------------------------------------------
 
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
 Solution Walkthrough
@@ -15,25 +46,12 @@ Ex.
 
 Return: true
 
-Time complexity: O(n). 
+Time complexity: O(1). 
 Space complexity: O(n). We are using a stack 
 ------------------------------------------------------------------------------------------------------------------------------------------------
 """
 
-"""
-Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
-
-Implement the MinStack class:
-
-MinStack() initializes the stack object.
-void push(int val) pushes the element val onto the stack.
-void pop() removes the element on the top of the stack.
-int top() gets the top element of the stack.
-int getMin() retrieves the minimum element in the stack.
-"""
-
 class MinStack:
-
     def __init__(self):
         self.stack = []
         self.min_stack = []
