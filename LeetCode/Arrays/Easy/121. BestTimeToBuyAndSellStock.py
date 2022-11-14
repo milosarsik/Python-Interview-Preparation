66c39c1d5fa879f76fd515a094632b816cf8ad93
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2022-11-14 14:34:28 -0500

    Update and rename BestTimeToBuyAndSellStock.py to 121. BestTimeToBuyAndSellStock.py

--- LeetCode/Arrays/Easy/121. BestTimeToBuyAndSellStock.py
+++ LeetCode/Arrays/Easy/121. BestTimeToBuyAndSellStock.py
@@ -48,31 +48,36 @@ Space complexity : O(1). Only two variables are used.
 ------------------------------------------------------------------------------------------------------------------------------------------------
 """
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        buy = prices[0]
-        max_profit = 0
-        
-        for i in range(1, len(prices)):
-            profit = prices[i] - buy
-            
-            if profit > max_profit:
-                max_profit = profit
-            if buy > prices[i]:
-                buy = prices[i]
-        
-        return max_profit
+def maxProfit(self, prices: List[int]) -> int:
+    res = 0
+    l = 0
+    for r in range(1, len(prices)):
+        if prices[r] < prices[l]:
+            l = r
+        res = max(res, prices[r] - prices[l])
+    return res
 
-"""
-EPI solution
-"""
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price_so_far, max_profit = float('inf'), 0
-        
-        for price in prices:
-            max_profit_sell_today = price - min_price_so_far
-            max_profit = max(max_profit, max_profit_sell_today)
-            min_price_so_far = min(min_price_so_far, price)
+def maxProfit(self, prices: List[int]) -> int:
+    buy = prices[0]
+    max_profit = 0
 
-        return max_profit
+    for i in range(1, len(prices)):
+        profit = prices[i] - buy
+
+        if profit > max_profit:
+            max_profit = profit
+        if buy > prices[i]:
+            buy = prices[i]
+
+    return max_profit
+
+# EPI Solution
+def maxProfit(self, prices: List[int]) -> int:
+    min_price_so_far, max_profit = float('inf'), 0
+
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+
+    return max_profit
