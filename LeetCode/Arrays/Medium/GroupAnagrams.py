98bde1a02a0b3fda07aa175ee6dc8cf37c3d924f
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2023-08-31 23:10:27 -0400

    ADD code for GroupAnagrams.py

--- LeetCode/Arrays/Medium/GroupAnagrams.py
+++ LeetCode/Arrays/Medium/GroupAnagrams.py
@@ -1,4 +1,7 @@
 """
+------------------------------------------------------------------------------------------------------------------------------------------------
+49. Group Anagrams (NeetCode)
+
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -10,6 +13,21 @@ Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
 Time complexity: O(m * n). 
 Space complexity: O(n). We are only taking up space for how long each word is. Im actually unsure of this time and space complexity 
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+Solution Walkthrough 
+
+We can solve this problem by using HashMap. However, the key will actually be a list that
+holds the count of the letters that we are searching for, and the value will be a list of 
+the words that contain those letters. Once we have gone through every string, we are going 
+to return the resulting array, which is an array of arrays containg the griouped anagrams.
+
+In the count array, the index will be calculated by subtraction the ord(character) by ord("a").
+
+Go over what the ordinal values are quickly if that doesnt make sense above.
+
+Time complexity: O(m * n).
+Space complexity: O(1).
 """
 
 class Solution:
